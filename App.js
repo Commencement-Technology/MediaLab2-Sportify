8458e5fa775b9de3e40c1933096b5db7d6a440ae
senalisa <senababacan@hotmail.com>
2024-05-28 15:27:23 +0200

    Installing dependencies

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NativeBaseProvider, Box, Input, Button, IconButton } from "native-base";
+import { View } from "react-native";
 
 export default function App() {
+  const Example = () => {
+    return (
+      <Box alignItems="center">
+        <Input mx="3" placeholder="Input" w="100%" />
+      </Box>
+    );
+  };
+
+  const Hallo = () => {
+    return <Box alignItems="center">
+        <Button onPress={() => console.log("hello world")}>Click Me</Button>
+      </Box>;
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <View className="flex-1 mt-20 mx-10">
+        <Example />
+        <Hallo />
+      </View>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: [
+      "./App.{js,jsx,ts,tsx}",
+      "./components/**/*.{js,jsx,ts,tsx}",
+      "./screens/*.{js,jsx,ts,tsx}",
+      "./navigation/*.{js,jsx,ts,tsx}",
+      // "./<custom directory>/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+      extend: {
+      colors: {
+          "dark-pink" : "#63254E",
+          "light-beige" : "#F5EDE5",
+          "dark-beige" : "#C8BBA8",
+          "pastel-green" : "#E2E0C1",
+
+        // Blue
+          "light-blue" : "#F4FBFD",
+          "blue" : "#C9E9F2",
+
+        //   Pink
+          "pink" : "#EBCDDC",
+          "pink-light" : "#EBDBE3",
+          "pastel-pink": "#E7B1AF",
+          "pinkie" : "#F5EBEF"
+      }
+      },
+  },
+  plugins: [],
+}
