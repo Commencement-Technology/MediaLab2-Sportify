54ba2fe288aec418f2a4cba5696e7b314bee542b
Bilal <89836993+Bilal2903@users.noreply.github.com>
2024-05-31 20:29:48 +0200

    Code cleanup

--- screens/LevelDetail.jsx
+++ screens/LevelDetail.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
-import Ionicons from "@expo/vector-icons/Ionicons";
 import { ScrollView } from "react-native-gesture-handler";
 
 const LevelDetail = () => {
