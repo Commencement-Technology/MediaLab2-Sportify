a82c6ebfd7f46ba372e56e5f30d293f536649487
Bilal <89836993+Bilal2903@users.noreply.github.com>
2024-06-03 19:27:28 +0200

    leaderboard done

--- screens/Leaderboard.jsx
+++ screens/Leaderboard.jsx
@@ -1,10 +1,77 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 
-export default function Leaderboard() {
+const Leaderboard = () => {
   return (
-    <View className="my-20 mx-10">
-      <Text>Leaderboard</Text>
+    <View className="h-full bg-white">
+      <View className="py-4 border-b border-gray-200 h-32">
+        <Text className="text-2xl font-bold text-left mt-16 ml-8 text-dark-blue">
+          Leaderboard
+        </Text>
+      </View>
+
+      <View className="bg-gray-200 flex-1">
+        <View className="flex flex-row justify-center items-center mt-4 mx-">
+          <View className="w-16 h-16 bg-white border border-gray-300 rounded-lg mx-2"></View>
+          <View className="w-16 h-16 bg-white border border-gray-300 rounded-lg mx-2"></View>
+          <View className="w-24 h-24 bg-white border border-gray-300 rounded-lg flex items-center justify-center mx-2"></View>
+          <View className="w-16 h-16 bg-white border border-gray-300 rounded-lg mx-2"></View>
+          <View className="w-16 h-16 bg-white border border-gray-300 rounded-lg mx-2"></View>
+        </View>
+
+        <View className="mt-6 mx-4">
+          <View className="flex items-center justify-center mb-4">
+            <Text className="text-lg font-semibold text-dark-blue">
+              Rank: Enthusiast
+            </Text>
+          </View>
+
+          <View className="flex-1 mb-4 px-4 mb-20">
+            <View className="w-full h-12 bg-gray-50 rounded-lg flex items-center justify-center">
+              <Text className="text-center">
+                ⏳ Nog <Text style={{ fontWeight: "bold" }}>26</Text> dagen en
+                <Text style={{ fontWeight: "bold" }}> 23</Text> uur
+              </Text>
+            </View>
+          </View>
+
+          <ScrollView>
+            {[
+              { id: 1, name: "Gebruiker 1", xp: "400 XP", days: "1+ Jaar" },
+              { id: 2, name: "Gebruiker 2", xp: "350 XP", days: "1+ Jaar" },
+              { id: 3, name: "Gebruiker 3", xp: "200 XP", days: "109 dagen" },
+              { id: 4, name: "Gebruiker 4", xp: "150 XP", days: "54 dagen" },
+            ].map((user, index) => (
+              <View
+                key={user.id}
+                className={`rounded-lg shadow-md bg-white p-4 mb-4 ${
+                  index !== 3 ? "border-b border-gray-300" : ""
+                }`}
+              >
+                <View
+                  key={user.id}
+                  className={`flex flex-row items-center justify-between`}
+                >
+                  <View className="w-12 h-12 bg-gray-300 rounded-lg shadow-md flex items-center justify-center">
+                    <Text className="text-lg font-bold text-black">
+                      {user.id}
+                    </Text>
+                  </View>
+                  <View className="flex-1 ml-4">
+                    <Text className="text-lg font-bold">{user.name}</Text>
+                    <View className="flex flex-row">
+                      <Text className="text-gray-500 mr-2">{user.xp}</Text>
+                      <Text className="text-gray-500">🔥 {user.days}</Text>
+                    </View>
+                  </View>
+                </View>
+              </View>
+            ))}
+          </ScrollView>
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
+
+export default Leaderboard;
